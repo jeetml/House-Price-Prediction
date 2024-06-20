ee4249f9f706471ae99bdcae9fc3de40f0b017fa
Jeet Patel <166617779+jeetml@users.noreply.github.com>
2024-06-20 10:38:03 +0530

    app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ location = sorted(data['location'].unique())
 location = st.selectbox('Location',location)
 size = st.number_input('Size (in square feet)', value=1000)
 bath = st.number_input('Number of Bathrooms', value=2, step=1)
-balcony = st.number_input('BHK', value=2, step=1)
+bhk = st.number_input('BHK', value=2, step=1)
 
 # Predict button
 if st.button('Predict'):
     # Create a DataFrame for the input
-    input_data = pd.DataFrame([[location, size, bath, BHK]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    input_data = pd.DataFrame([[location, size, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
     # Make a prediction
     prediction = pipe.predict(input_data)[0]*100000
     # Display the prediction
